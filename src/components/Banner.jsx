c52d69e1a7fac15f7152a05582877959274dfcb3
younes BENNACEUR <your-new-email@example.com>
2025-05-03 20:36:17 +0200

    styling the banner section

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,22 +3,22 @@ import {assets} from '../assets/assets'
 
 const Banner = () => {
   return (
-    <div>
+    <div className='flex flex-col md:flex-row justify-between rounded-lg justify-center items-center bg-[var(--primary-color)] px-24 py-24'>
 
     
-    <div>
-      <div>
-      <p>Book Appointment</p>
-      <p>With 100+ Trusted Doctors</p>
+    <div className='flex w-2/3 flex-col gap-8'>
+      <div className='flex flex-col gap-4'>
+      <p className='text-5xl font-semibold  text-white '>Book Appointment</p>
+      <p className='text-5xl font-semibold  text-white'>With 100+ Trusted Doctors</p>
       </div>
      
-      <button>Create Account</button>
+      <button className=' w-1/4 bg-white  px-6 py-3 rounded-lg font-normal transition-colors duration-300 hover:bg-blue-50'>Create Account</button>
 
 
     </div>
 
-    <div>
-      <img src={assets.appointment_img} alt="" />
+    <div className='hidden md:block md:w-1/3 relative'>
+      <img className=' w-95 h-105 absolute top-[-228px] right-0  ' src={assets.appointment_img} alt="" /> 
 
 
     </div>
